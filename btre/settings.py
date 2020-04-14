52603d857d61309da2b746f357d4b3642202b742
ahb10 <61655544+ahb10@users.noreply.github.com>
2020-04-14 09:07:08 +0500

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ahbaig9@gmail.com'
+EMAIL_HOST_USER = '*****'
 EMAIL_HOST_PASSWORD = '*****'
 EMAIL_USE_TLS = True
